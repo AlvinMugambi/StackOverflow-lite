cfffe706b523724e7dc8060b1e500f9409b63333
AlvinMugambi <ngurual123@gmail.com>
2018-12-14 23:17:04 +0300

    [feature #162672905] Wrote the create user function that appends a user to the users list

--- API/app.py
+++ API/app.py
@@ -8,7 +8,33 @@ app= Flask(__name__)
 
 app.config['SECRET_KEY']= 'thisissecret'
 
+@app.route('/register', methods=['POST'])
+def create_user():
+    user= {}
+    name = request.get_json()['name']
+    email= request.get_json()['email']
+    password=request.get_json()['password']
+    public_id=str(uuid.uuid4())
 
+    if not name or not email or not password:
+        response = jsonify({"error": "Name, email and password fields required"})
+        return response, 400
+    new_user= verify_user_email(email)
+    print("USER >> ", new_user)
+    if new_user == 'Email already taken!':
+        return jsonify({'error': 'Email already taken!'})
+
+    hashed_password = generate_password_hash(password,method= 'sha256')
+
+    user = {
+        'name':name,
+        'email':email,
+        'password':hashed_password,
+        'public_id':public_id
+        }
+    users.append(user)
+
+    return jsonify({'User':users}), 201
 
 
 if __name__ == '__main__':
