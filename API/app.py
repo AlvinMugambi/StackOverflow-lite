423466f7d844c7ec141c6216dae47ce2336094df
AlvinMugambi <ngurual123@gmail.com>
2018-12-16 11:23:07 +0300

    [finishes #162685807]Wrote the function that allows one to get a specific question

--- API/app.py
+++ API/app.py
@@ -128,7 +128,15 @@ def get_all_questions():
     # a user can search for a specific # QUESTION:
 @app.route('/find/<int:question_id>', methods= ['GET'])
 def view_question(question_id):
-    return ''
+    
+    clicked_question = None
+    for question in questions:
+        if question['id'] == question_id:
+            clicked_question= question
+    if not clicked_question:
+        return "message=Question with id {} not found".format(question_id)
+
+    return jsonify({"Question" :clicked_question}),201
 
 if __name__ == '__main__':
     app.run(debug=True)
