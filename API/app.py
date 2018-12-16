1f5764716f6ec6051b839e9c13ff4f8e7920d980
AlvinMugambi <ngurual123@gmail.com>
2018-12-16 11:30:42 +0300

    [start #162673040]Initiated the post answer function

--- API/app.py
+++ API/app.py
@@ -128,7 +128,7 @@ def get_all_questions():
     # a user can search for a specific # QUESTION:
 @app.route('/find/<int:question_id>', methods= ['GET'])
 def view_question(question_id):
-    
+
     clicked_question = None
     for question in questions:
         if question['id'] == question_id:
@@ -137,6 +137,13 @@ def view_question(question_id):
         return "message=Question with id {} not found".format(question_id)
 
     return jsonify({"Question" :clicked_question}),201
+
+
+    # a registered user can post an answer to a question
+@app.route('/answer/<int:question_id>', methods=['POST'])
+@token_required
+def answer_question(current_user,question_id):
+    return ''
 
 if __name__ == '__main__':
     app.run(debug=True)
